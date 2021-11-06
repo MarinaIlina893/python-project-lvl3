8f34f058e1895289d06f01b2a869bc769eca2108
MarinaIlina893 <marinailina893@gmail.com>
2021-11-06 21:51:55 +0500

    add image parcer

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,7 +1,9 @@
 import requests
+import os
 from urllib.parse import urlparse
-from os.path import join
+from os.path import join, splitext, split
 import re
+from bs4 import BeautifulSoup
 
 
 def download(url, directory):
@@ -9,8 +11,40 @@ def download(url, directory):
     filename = re.sub(r'[^A-Za-z0-9]',
                       '-',
                       parsed_url.netloc + parsed_url.path) + '.html'
+    resource_directory = join(directory, splitext(filename)[0] + '_files')
     filepath = join(directory, filename)
     content = requests.get(url).text
+    download_images(content, resource_directory, url)
     with open(filepath, 'w') as file:
-        file.write(content)
+        file.write(update_image_links(content,
+                                      split(resource_directory)[1],
+                                      url))
     return filepath
+
+
+def download_images(content, directory, page_url):
+    soup = BeautifulSoup(content, 'html.parser')
+    images = soup.findAll('img')
+    for image in images:
+        u = urlparse(page_url)
+        url = image['src']
+        name = re.sub(r'[^A-Za-z0-9]',
+                      '-',
+                      u.netloc + splitext(image['src'])[0])
+        extension = splitext(url)[1]
+        os.mkdir(directory)
+        filepath = join(directory, name) + extension
+        with open(filepath, 'wb') as file:
+            image_url = u.scheme + '://' + u.netloc + url
+            file.write(requests.get(image_url).content)
+
+
+def update_image_links(content, directory, page_url):
+    soup = BeautifulSoup(content, 'html.parser')
+    parsed_url = urlparse(page_url)
+    for image in soup.findAll("img"):
+        name = re.sub(r'[^A-Za-z0-9]', '-', parsed_url.netloc) + \
+            re.sub(r'[^A-Za-z0-9]', '-', splitext(image['src'])[0])
+        extension = splitext(image['src'])[1]
+        image['src'] = join(directory, name) + extension
+    return soup.prettify()
--- tests/test_download.py
+++ tests/test_download.py
@@ -2,25 +2,30 @@ from pytest import fixture
 from requests_mock import Mocker
 from tempfile import TemporaryDirectory
 from page_loader.page_loader import download
+from bs4 import BeautifulSoup
 
 
 @fixture
 def mocker():
     mocker = Mocker()
-    mocker.register_uri('GET', 'http://test.com', text='data')
+    with open('tests/fixtures/raw_data.html') as page:
+        mocker.register_uri('GET', 'http://test.com', text=page.read())
+        mocker.register_uri('GET', 'http://test.com/assets/professions/nodejs.png')
     return mocker
 
 
-def test_download(mocker):
+def test_download_filepath(mocker):
     with mocker:
         with TemporaryDirectory() as tmpdir:
             r = download('http://test.com', tmpdir)
             assert r == tmpdir + '/test-com.html'
 
 
-def test_file_content(mocker):
+def test_download_file_content(mocker):
     with mocker:
         with TemporaryDirectory() as tmpdir:
             r = download('http://test.com', tmpdir)
             with open(r, 'r') as file:
-                assert file.read() == 'data'
+                with open('tests/fixtures/parsed_data.html') as parsed_page:
+                    soup = BeautifulSoup(parsed_page.read(),  'html.parser')
+                    assert file.read() == soup.prettify()
