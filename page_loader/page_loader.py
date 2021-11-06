7ebb5895b4f1c35437ee8c6f0f6be7e592319886
MarinaIlina893 <marinailina893@gmail.com>
2021-11-06 23:39:37 +0500

    add link and script parcer

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -16,9 +16,9 @@ def download(url, directory):
     content = requests.get(url).text
     download_images(content, resource_directory, url)
     with open(filepath, 'w') as file:
-        file.write(update_image_links(content,
-                                      split(resource_directory)[1],
-                                      url))
+        file.write(update_resource_links(content,
+                                         split(resource_directory)[1],
+                                         url))
     return filepath
 
 
@@ -39,7 +39,26 @@ def download_images(content, directory, page_url):
             file.write(requests.get(image_url).content)
 
 
-def update_image_links(content, directory, page_url):
+def download_scripts(content, directory, page_url):
+    soup = BeautifulSoup(content, 'html.parser')
+    scripts = soup.findAll('script')
+    for script in scripts:
+        u = urlparse(page_url)
+        if script.get('src') is not None:
+            src = urlparse(script['src'])
+            if src.netloc == u.netloc:
+                url = script['src']
+                name = re.sub(r'[^A-Za-z0-9]',
+                              '-',
+                              u.netloc + splitext(script['src'])[0])
+                extension = splitext(url)[1]
+                filepath = join(directory, name) + extension
+                with open(filepath, 'w') as file:
+                    script_url = u.scheme + '://' + u.netloc + url
+                    file.write(requests.get(script_url).content)
+
+
+def update_resource_links(content, directory, page_url):
     soup = BeautifulSoup(content, 'html.parser')
     parsed_url = urlparse(page_url)
     for image in soup.findAll("img"):
@@ -47,4 +66,44 @@ def update_image_links(content, directory, page_url):
             re.sub(r'[^A-Za-z0-9]', '-', splitext(image['src'])[0])
         extension = splitext(image['src'])[1]
         image['src'] = join(directory, name) + extension
+    for script in soup.findAll("script"):
+        if script.get('src') is not None:
+            src = urlparse(script['src'])
+            page_host = urlparse(page_url).netloc
+            if src.netloc == page_host:
+                name = re.sub(r'[^A-Za-z0-9]', '-', parsed_url.netloc) + \
+                    re.sub(r'[^A-Za-z0-9]',
+                           '-',
+                           urlparse(splitext(script['src'])[0]).path)
+                extension = splitext(script['src'])[1]
+                script['src'] = join(directory, name) + extension
+    for link in soup.findAll("link"):
+        page_host = urlparse(page_url)
+        href = urlparse(link['href'])
+        if page_host.netloc == href.netloc or href.netloc == '':
+            name = re.sub(r'[^A-Za-z0-9]', '-', parsed_url.netloc) + \
+                re.sub(r'[^A-Za-z0-9]', '-', splitext(link['href'])[0])
+            extension = splitext(link['href'])[1]
+            if extension == '':
+                extension = '.html'
+            link['href'] = join(directory, name) + extension
     return soup.prettify()
+
+
+def download_links(content, directory, page_url):
+    soup = BeautifulSoup(content, 'html.parser')
+    links = soup.findAll('link')
+    for link in links:
+        u = urlparse(page_url)
+        if link.get('src') is not None:
+            href = urlparse(link['href'])
+            if href.netloc == u.netloc:
+                url = link['href']
+                name = re.sub(r'[^A-Za-z0-9]',
+                              '-',
+                              u.netloc + splitext(link['href'])[0])
+                extension = splitext(url)[1]
+                filepath = join(directory, name) + extension
+                with open(filepath, 'w') as file:
+                    link_href = u.scheme + '://' + u.netloc + url
+                    file.write(requests.get(link_href).content)
--- tests/test_download.py
+++ tests/test_download.py
@@ -9,22 +9,23 @@ from bs4 import BeautifulSoup
 def mocker():
     mocker = Mocker()
     with open('tests/fixtures/raw_data.html') as page:
-        mocker.register_uri('GET', 'http://test.com', text=page.read())
-        mocker.register_uri('GET', 'http://test.com/assets/professions/nodejs.png')
+        mocker.register_uri('GET', 'http://ru.hexlet.io/courses', text=page.read())
+        mocker.register_uri('GET', 'http://ru.hexlet.io/assets/professions/nodejs.png')
+        mocker.register_uri('GET', 'https://ru.hexlet.io/packs/js/runtime.js')
     return mocker
 
 
 def test_download_filepath(mocker):
     with mocker:
         with TemporaryDirectory() as tmpdir:
-            r = download('http://test.com', tmpdir)
-            assert r == tmpdir + '/test-com.html'
+            r = download('http://ru.hexlet.io/courses', tmpdir)
+            assert r == tmpdir + '/ru-hexlet-io-courses.html'
 
 
 def test_download_file_content(mocker):
     with mocker:
         with TemporaryDirectory() as tmpdir:
-            r = download('http://test.com', tmpdir)
+            r = download('http://ru.hexlet.io/courses', tmpdir)
             with open(r, 'r') as file:
                 with open('tests/fixtures/parsed_data.html') as parsed_page:
                     soup = BeautifulSoup(parsed_page.read(),  'html.parser')
